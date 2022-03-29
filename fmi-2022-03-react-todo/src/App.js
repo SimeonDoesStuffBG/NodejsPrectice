3a1047e005c74699d3069c8c4e9acd7998dc22c7
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-03-29 19:38:04 +0300

    ddd

--- fmi-2022-03-react-todo/src/App.js
+++ fmi-2022-03-react-todo/src/App.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 import './App.css';
+import TodoFilter from './components/TodoFilter';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import MOCK_TODOS from './model/mock-todos';
 
 function App() {
-  const [todos, setTodos] = useState(MOCK_TODOS);
+  const [todos, setTodos] = useState(MOCK_TODOS)
+  function deleteTodo(deleted) {
+    setTodos(oldTodos => oldTodos.filter(td => td.id !== deleted.id));
+  }
+  function statusChanged(changedTodo) {
+    setTodos(oldTodos => oldTodos.map(td => td.id === changedTodo.id? changedTodo : td))
+  }
+  function filterChanged(filterText){
+    //You code here
+  }
   return (
     <div className="App-header">
       <h1>React TODOs Demo</h1>
-      <TodoInput onSubmitTodo={todo => setTodos(oldTodos => [...oldTodos,todo])}/>
-      <TodoList todos={todos} />
+      <TodoInput onSubmitTodo={todo => setTodos(oldTodos => [...oldTodos, todo])} />
+      <TodoFilter onFilterChange={filterChanged} />
+      <TodoList todos={todos} onDelete={deleteTodo} onStatusChange={statusChanged}/>
     </div>
   );
 }
--- fmi-2022-03-react-todo/src/components/TodoItem.js
+++ fmi-2022-03-react-todo/src/components/TodoItem.js
@@ -1,9 +1,12 @@
 import './TodoItem.css'
 import React from 'react'
 import PropTypes from 'prop-types'
-import { TodoStatus } from '../model/todo-model'
+import { ALL_STATUSES, ACTIVE, CANCELED, COMPLETED, TodoStatus } from '../model/todo-model'
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, onDelete, onStatusChange }) => {
+    function statusChanged(newStatus) {
+        onStatusChange({ ...todo, status: newStatus })
+    }
     return (
         <div className="TodoItem">
             <span className='TodoItem-text'>
@@ -16,15 +19,25 @@ const TodoItem = ({ todo }) => {
                 <span className='TodoItem-status'>
                     {TodoStatus[todo.status]}
                 </span>
-
-                <span>
-                    <button type = "button" onClick = {() => {}}>DELETE</button>
-                </span>
-                </span>
+                <span className="TodoItem-button fas fa-check-circle" title="Complete Todo"
+                    onClick={() => statusChanged(COMPLETED)} />
+                <span className="TodoItem-button danger fas fa-ban" title="Cancel Todo"
+                    onClick={() => statusChanged(CANCELED)} />
+                <span className="TodoItem-button danger fas fa-times-circle" title="Delete Todo"
+                    onClick={() => onDelete(todo)} />
+            </span>
         </div>
-  )
+    )
 }
 
-TodoItem.propTypes = {}
+TodoItem.propTypes = {
+    todo: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        status: PropTypes.oneOf([ALL_STATUSES, ACTIVE, COMPLETED, CANCELED])
+    }),
+    onDelete: PropTypes.func.isRequired,
+    onStatusChange: PropTypes.func.isRequired
+}
 
 export default TodoItem
