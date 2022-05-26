0c8f9c4d3bb33dd8b53827473ef55afdecd25f7a
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-05-26 21:52:23 +0300

    added Task and TaksList

--- travesty-media-tutorial/src/App.js
+++ travesty-media-tutorial/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import {useState} from 'react'
 
 function App() {
+
+  const [tasks,setTasks]=useState([{id:1,text:'doctor', day:'May 27', reminder:true}])
+
+
   return (
     <div className="App">
       <Header title = "Add"/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
