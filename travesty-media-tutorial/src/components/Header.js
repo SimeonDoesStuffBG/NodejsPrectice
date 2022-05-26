c284f16add3fd59ea11c72c1e004d1ac747a4d5d
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-05-26 20:40:41 +0300

    Better style + header

---
+++ travesty-media-tutorial/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <header>
+        <h1>Task Tracker</h1>
+        <h2>{props.title}</h2>
+    </header>
+  )
+}
+
+export default Header
+
