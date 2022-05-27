1458c338322cd022af5577b75f5054ff7e2ceffa
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-05-28 00:01:56 +0300

    added Server, about & footer

--- travesty-media-tutorial/src/App.js
+++ travesty-media-tutorial/src/App.js
@@ -2,26 +2,81 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
 
 function App() {
 
   const [showAdd,setShowAdd] = useState(false);
-  const [tasks,setTasks]=useState([{id:1,text:'doctor', day:'May 27', reminder:true}])
 
-  const addTask = (task)=>{
-    const id = tasks.length+1;
-    const newTask = {id, ...task};
+  const [tasks,setTasks]=useState([]);
+
+  useEffect(() => {
+    const getTasks = async() =>{
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+
+    getTasks();
+  }, [])
+  
+  const fetchTasks = async()=>{
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json();
+
+    return data;
+  }
+
+  const fetchTask = async(id)=>{
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json();
+
+    return data;
+  }
+
+
+  const addTask = async (task)=>{
+    /*const id = tasks.length+1;
+    const newTask = {id, ...task};*/
+    const res = await fetch('http://localhost:5000/tasks',{ 
+      method:'POST',
+      headers:{ 
+        'Content-type':'application/json'
+      },
+      body:JSON.stringify(task)
+    });
+
+    const newTask =await res.json();
     setTasks([...tasks, newTask]);
   }
 
-  const deleteTask = (id)=>{
+  const deleteTask = async (id)=>{
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'DELETE',
+    });
+
     setTasks(tasks.filter((task)=>task.id!==id))
   }
 
-  const toggleReminder=(id)=>{
-    setTasks(tasks.map((task)=> task.id===id?{...task,reminder:!task.reminder}:task))
+  const toggleReminder= async (id)=>{
+    const taskToUpdate = await fetchTask(id);
+    const uppTask = {...taskToUpdate, reminder:!taskToUpdate.reminder};
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'PUT',
+      headers:{
+        'Content-type':'application/json',
+      },
+      body:JSON.stringify(uppTask)
+    })
+
+    const data = await res.json();
+
+    setTasks(
+      tasks.map((task)=> 
+      task.id===id?{...task, reminder:
+      data.reminder}:task))
   }
 
   return (
@@ -29,6 +84,7 @@ function App() {
       <Header onAddTask={()=>setShowAdd(!showAdd)} showAdd={showAdd}/>
       {showAdd && <AddTask onAdd={addTask}/>}
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      <Footer/>
     </div>
   );
 }
---
+++ travesty-media-tutorial/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Why hello there.</p>
+        <a href="/about">Cool shit</a>
+    </footer>
+  )
+}
+
+export default Footer
