4710711f419b297d946068ff87cbdd04a5cdf5ea
SimeonDoesStuffBG <44085498+SimeonDoesStuffBG@users.noreply.github.com>
2022-05-31 14:23:39 +0300

    index.html

--- Expressjs course/index.js
+++ Expressjs course/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('<p>hi<p>');
+const members = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      status: 'active'
+    },
+    {
+      id: 2,
+      name: 'Bob Williams',
+      email: 'bob@gmail.com',
+      status: 'inactive'
+    },
+    {
+      id: 3,
+      name: 'Shannon Jackson',
+      email: 'shannon@gmail.com',
+      status: 'active'
+    }
+  ];
+
+app.get('/api/members',(req,res)=>{
+    res.json(members);
+
 });
+
+app.use(express.static(path.join(__dirname,'public')));
+
+//app.get('/', (req, res)=>{
+    //res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//});
 
 
 const PORT = process.env.PORT||5000;
